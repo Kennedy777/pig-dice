8061e08a1a94d4bb8eb3483d1d251f4d235b1980
Maya Frame and Marguerite Kennedy <marguerite@>
2019-04-10 15:40:56 -0700

    add working functions and objects for dice roll and two players.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
 //Business end
-function RollScore() {
-var rollArray = [];
-var roll = Math.floor((Math.random() * 6) + 1);
-  rollArray.push(roll);
+
+//Globals
+var rollDice = 0;
+var total = 0;
+var scorePlayer1 = 0;
+var scorePlayer2 = 0;
+//Rolling the dice
+function Roll(){
+  rollDice = Math.floor((Math.random() * 6) + 1);
 }
-    
-    
+function Total(){
+    if(rollDice === 1){
+      total = 0;
+      alert("Oh no! You rolled a 1. Your turn is over. And you are awarded no points.")
+    } else {
+      total += rollDice;
+    }    
+  return total;
+}
 //Front/User end
 $(document).ready(function() {
   $("#rollDice").click(function(event) {
-    roll();
-  }
     event.preventDefault();
-    var  = parseInt($("#add1").val());
-    var result = add(number1, number2);
-    $("#rollOutput").text(result);
-})
+    Roll();
+    $("#rollOutput").html(rollDice);
+    $("#turnTotal").html(Total());
+    
+  });
+//Player 1
+  $("#player1").click(function(event){
+    scorePlayer1 += total;
+    $("#score1").html(scorePlayer1);
+    total = 0;
+    rollDice = 0;
+    $("#rollOutput").html(total);
+    $("#turnTotal").html(rollDice);
+  });
+  $("#player2").click(function(event) {
+    scorePlayer2 += total;
+    $("#score2").html(scorePlayer2);
+    total = 0;
+    rollDice = 0;
+    $("#rollOutput").html(total);
+    $("#turnTotal").html(total);
+  })
+
 });
