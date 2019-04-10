23768ff08513966eab734552ecfc2bb71b2fe63c
Maya Frame and Marguerite Kennedy <marguerite@>
2019-04-10 11:18:29 -0700

    add working random number generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 //Business end
-
+function RollScore() {
+var rollArray = [];
+var roll = Math.floor((Math.random() * 6) + 1);
+  rollArray.push(roll);
+}
+    
+    
 //Front/User end
 $(document).ready(function() {
-  $("form#new-roll").submit(function(event) {
+  $("#rollDice").click(function(event) {
+    roll();
+  }
     event.preventDefault();
-  })
+    var  = parseInt($("#add1").val());
+    var result = add(number1, number2);
+    $("#rollOutput").text(result);
 })
+});
